cecb76d7cb88349dcc2853d30ca49940e79990c5
Shashank Singh <168545210+Imsainthwar@users.noreply.github.com>
2025-07-23 03:28:44 +0530

    Add files via upload

---
+++ _13Precedence.java
@@ -0,0 +1,9 @@
+public class _13Precedence {
+    public static void main(String[] args)
+{
+System.out.println(8+2*(3-2));
+}
+
+}
+
+
---
+++ _14Shorthand.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class _14Shorthand {
+
+
+    public static void main(String[] args) {
+        int a = 5;
+    Scanner input = new Scanner (System.in);
+     int x1 = input.nextInt();
+     System.out.println("Enter the number");
+     a += x1;
+   System.out.println(a);
+   
+}
+    
+}
